40aaef53ae2c34e62a21d728366e70120067068a
Tokxhu <thomas.granbohm@hotmail.com>
2021-06-09 17:17:32 +0200

    🚀 Add production compose file

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: env("URL", "http://localhost:1337/api"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "43b1fe8cdec93f5ca76b728ce718cf5e"),
