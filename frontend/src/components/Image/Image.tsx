8ae0bf69ed60361abe23530e33f55e287b1ed229
Thomas Granbohm <thomas.granbohm@gmail.com>
2021-06-11 16:13:51 +0200

    🎨 Updated various styles

--- frontend/src/components/Image/Image.tsx
+++ frontend/src/components/Image/Image.tsx
@@ -1,6 +1,5 @@
 import React, { ImgHTMLAttributes } from "react";
 import concatClasses from "../../functions/concatClasses";
-import NextImage from "next/image";
 import { getStrapiMedia } from "../../utils/media";
 
 type ImageProps = {
@@ -12,8 +11,6 @@ export const Image = ({ alt, className, src, ...props }: ImageProps) => {
 		<img
 			alt={alt}
 			src={getStrapiMedia(src)}
-			height={128}
-			width={128}
 			className={concatClasses([className, className])}
 			{...props}
 		/>
