a5a58f91068003be8951563d8349f36e2d9582dd
Thomas Granbohm <thomas.granbohm@gmail.com>
2022-05-15 16:22:51 +0200

    Added GTM support

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -25,10 +25,16 @@ class CustomDocument extends Document {
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
 					<link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;600;700&display=swap" rel="stylesheet" />
+
+					{/* Google Tag Manager */}
+					<script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${process.env.GTM_TAG}');`}}></script>
 				</Head>
 				<body>
 					<Main />
 					<NextScript />
+					
+					{/* Google Tag Manager */}
+					<noscript><iframe src={`https://www.googletagmanager.com/ns.html?id=${process.env.GTM_TAG}`} height="0" width="0" style={{display: "none", visibility: "hidden"}}></iframe></noscript>
 				</body>
 			</Html>
 		);
